df5c2e3188e440beb5a2f4d08766330674c7c506
K.Roshani <k.roshani2003@gmail.com>
2026-07-16 22:01:55 +0530

    Initial commit

---
+++ heart.py
@@ -0,0 +1,9 @@
+for y in range(15, -15, -1):
+    for x in range(-30, 30):
+        formula = ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3
+
+        if formula <= 0:
+            print("❤️", end="")
+        else:
+            print("  ", end="")
+    print()
