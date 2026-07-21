8972f67af0db8339a596ad381ea62df1330de9a6
K.Roshani <k.roshani2003@gmail.com>
2026-07-21 06:01:16 +0530

    Add

---
+++ CustomerPhoneNumber.py
@@ -0,0 +1,12 @@
+
+customer_name = input("Enter Customer Name : ")
+phone_number = input("Enter Phone Number : ")
+
+print("\n")
+print("=" * 50)
+print("              FINAL RECEIPT")
+print("=" * 50)
+
+print("Customer Name :", customer_name)
+print("Phone Number  :", phone_number)
+print("-" * 50)
